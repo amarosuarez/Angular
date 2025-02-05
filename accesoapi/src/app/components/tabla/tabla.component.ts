47c11fe5bf1ca03af2c44af283e35402dce93cfc
amsuv <amaro.suarez@iesnervion.es>
2025-02-05 22:39:58 +0100

    haciendo el dialog para añadir a una persona

--- accesoapi/src/app/components/tabla/tabla.component.ts
+++ accesoapi/src/app/components/tabla/tabla.component.ts
@@ -1,12 +1,62 @@
 import { CommonModule, NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { Persona } from '../../interfaces/persona';
 import { PersonasService } from '../../services/personas.service';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+@Component({
+  selector: 'dialog',
+  templateUrl: './dialog.html',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+})
+export class DialogOverviewExampleDialog {
+  readonly dialogRef = inject(MatDialogRef<DialogOverviewExampleDialog>);
+  readonly data = inject<Persona>(MAT_DIALOG_DATA);
+  readonly persona = model(this.data);
+  readonly nombrePersona = model(this.data.nombre);
+  readonly apellidosPersona = model(this.data.apellidos);
+  readonly direccionPersona = model(this.data.direccion);
+  readonly fotoPersona = model(this.data.foto);
+  readonly telefonoPersona = model(this.data.telefono);
+  readonly fechaNacimientoPersona = model(this.data.fechaNacimiento);
+  readonly idDepartamentoPersona = model(this.data.idDepartamento);
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
 
 @Component({
   selector: 'app-tabla',
   // imports: [RouterOutlet, RouterLink, RouterLinkActive],
-  imports: [NgFor, CommonModule],
+  imports: [NgFor, CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './tabla.component.html',
   styleUrl: './tabla.component.scss'
 })
@@ -19,6 +69,22 @@ export class TablaComponent {
 
     this.obtenerPersonas();
 
+  }
+
+  readonly animal = signal('');
+  readonly name = model('');
+  readonly dialog = inject(MatDialog);
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: {name: this.name(), animal: this.animal()},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if (result !== undefined) {
+        this.animal.set(result);
+      }
+    });
   }
 
   async obtenerPersonas() {
